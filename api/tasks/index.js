51f5cb0285104fbf4c0cc863e5b4b2a2fabdc8fe
Juan <jjrosalesuci@gmail.com>
2020-08-13 18:24:31 -0300

    Fix status

--- api/tasks/index.js
+++ api/tasks/index.js
@@ -4,21 +4,21 @@ const taskService = require('../../services/tasks');
 router.post('/', (req, res) => {
     const newTask = req.body;
     taskService.add(newTask);
-    return res.sendStatus(201).end();
+    return res.sendStatus(201);
 });
 
 router.put('/:id', (req, res) => {
     const raskId = req.params.id;
     const task = req.body;
     if (taskService.update(raskId, task)) {
-      return res.sendStatus(200).end();
+      return res.sendStatus(200);
     }
-    return res.sendStatus(204).end();
+    return res.sendStatus(204);
 });
 
 router.get('/', (req, res) => {
     const taskList = taskService.getTasks();
-    return res.send(taskList);
+    return res.json(taskList);
 });
 
 router.get('/:id', (req, res) => {
